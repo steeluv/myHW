a152ed6f04a97eddfecfd25e4882469e299f250d
User <Oleg1Babchenok1@gmail.com>
2022-09-27 19:42:04 +0300

    recreated everything

---
+++ dictTASKS/Task5.py
@@ -0,0 +1,10 @@
+"""
+Дан словарь email-адресов студентов, в качестве ключа используется домен, а в качестве значения список имен.
+Необходимо вывести все email-адреса в формате Имя@домен.
+"""
+emails = {'mgu.edu': ['andrei_serov', 'alexander_pushkin', 'elena_belova', 'kirill_stepanov'],
+      	'gmail.com': ['alena.semyonova', 'ivan.polekhin', 'marina_abrabova'],
+      	'msu.edu': ['sergei.zharkov', 'julia_lyubimova', 'vitaliy.smirnoff'],
+      	'yandex.ru': ['ekaterina_ivanova', 'glebova_nastya'],
+      	'harvard.edu': ['john.doe', 'mark.zuckerberg', 'helen_hunt'],
+      	'mail.ru': ['roman.kolosov', 'ilya_gromov', 'masha.yashkina']}
