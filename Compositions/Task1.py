179f2020d29a75700754a00c5ce18ce846c8e3aa
User <Oleg1Babchenok1@gmail.com>
2022-11-23 11:07:00 +0300

    compositions added

---
+++ Compositions/Task1.py
@@ -0,0 +1,9 @@
+"""
+Создайте композицию User состояющую из:
+Объекта Profile со свойствами: name,last_name,age,pasport и методом print_info.
+Объекта Address со свойствами : City,street,zipcode
+Объекта Role со свойствами: role,hours_worked
+Объекта BankAccount со свойствами: card_number, balance
+Объекта Order с методом устанавливающими параметры заказа: Item,date,delivery,price
+Добавьте в композицию методы создания профиля, установки адреса, роли, привязки банковского аккаунта и добавления заказа
+"""
