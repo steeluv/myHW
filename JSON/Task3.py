247b3c8740580fe120f8a9949f88b8cdb05e0073
User <Oleg1Babchenok1@gmail.com>
2022-12-04 15:23:19 +0300

    json added

---
+++ JSON/Task3.py
@@ -0,0 +1,13 @@
+"""
+Сохраните данные из списка в json файл(Имя файла - ваша фамилия и номер задания) с отступом 4, формата:
+name: ***
+age: ***
+countries: [
+{
+name:***
+time:***
+cities:***
+}
+]
+"""
+task = ["oleg",24,["Belarus","Russia"],(24,1),["Moscow","Vladikavkaz",'Krasnodar',"Rostov","Nalchik"]]
